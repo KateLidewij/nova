2298da9c378d7d9c7876a1d5c195f463992217ea
David VanScott <david.vanscott@gmail.com>
2013-01-14 16:36:52 -0500

    Update the default skin

--- application/views/default/template_main.php
+++ application/views/default/template_main.php
@@ -1,6 +1,6 @@
 <?php
 
-$sec = 'main';
+$section = 'main';
 $css = 'main.css';
 
 $path = explode('/', dirname(__FILE__));
@@ -10,17 +10,14 @@ if (count($path) <= 1)
 	$path = explode('\\', dirname(__FILE__));
 
 $pcount = count($path);
-$skin_loc = $pcount -1;
-$current_skin = $path[$skin_loc];
-
-// set the final style location
-$style_loc = APPFOLDER.'/views/'.$current_skin.'/'.$sec.'/css/'. $css;
+$skinLoc = $pcount -1;
+$currentSkin = $path[$skinLoc];
 
 // set up the link tag parameters
 $link = array(
-	'href'	=> 	$style_loc,
-	'rel'	=> 	'stylesheet',
-	'type'	=> 	'text/css',
+	'href'		=> APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/css/'. $css,
+	'rel'		=> 'stylesheet',
+	'type'		=> 'text/css',
 	'media'		=> 'screen',
 	'charset'	=> 'utf-8'
 );
@@ -30,17 +27,17 @@ $this->load->helper('panel');
 
 // set up the locations of the icons
 $panel = array(
-	'inbox'		=> array('src' => APPFOLDER.'/views/'.$current_skin.'/'.$sec.'/images/panel-mail.png'),
-	'writing'	=> array('src' => APPFOLDER.'/views/'.$current_skin.'/'.$sec.'/images/panel-writing.png'),
-	'dashboard'	=> array('src' => APPFOLDER.'/views/'.$current_skin.'/'.$sec.'/images/panel-dashboard.png'),
+	'inbox'		=> array('src' => APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/images/panel-mail.png'),
+	'writing'	=> array('src' => APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/images/panel-writing.png'),
+	'dashboard'	=> array('src' => APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/images/panel-dashboard.png'),
 );
 
-$button_login = array(
-	'class' => 'button-signin',
-	'value' => 'submit',
-	'type' => 'submit',
-	'name' => 'submit',
-	'content' => ucwords(lang('actions_login'))
+$loginButton = array(
+	'class'		=> 'button-signin',
+	'value'		=> 'submit',
+	'type'		=> 'submit',
+	'name'		=> 'submit',
+	'content'	=> ucwords(lang('actions_login'))
 );
 
 ?><!DOCTYPE html>
@@ -49,11 +46,13 @@ $button_login = array(
 		<meta charset="utf-8">
 		<title><?php echo $title;?></title>
 		
-		<meta name="description" content="<?php echo $this->config->item('meta_desc');?>" />
-		<meta name="keywords" content="<?php echo $this->config->item('meta_keywords');?>" />
-		<meta name="author" content="<?php echo $this->config->item('meta_author');?>" />
+		<meta name="description" content="<?php echo $this->config->item('meta_desc');?>">
+		<meta name="keywords" content="<?php echo $this->config->item('meta_keywords');?>">
+		<meta name="author" content="<?php echo $this->config->item('meta_author');?>">
 		
 		<?php echo $_redirect;?>
+
+		<?php include_once $this->config->item('include_head_main');?>
 		
 		<?php echo link_tag($link);?>
 		
@@ -61,9 +60,7 @@ $button_login = array(
 		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		
-		<?php include_once($this->config->item('include_head_main'));?>
-		
-		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER;?>/views/<?php echo $current_skin;?>/jquery.blockUI.js"></script>
+		<script type="text/javascript" src="<?php echo base_url().APPFOLDER;?>/views/<?php echo $currentSkin;?>/jquery.blockUI.js"></script>
 		
 		<?php echo $javascript;?>
 		
@@ -124,80 +121,34 @@ $button_login = array(
 				</div>
 			</div>
 		<?php endif;?>
-		
+
 		<header>
-			<div class="wrapper">
-				<div class="signin-container">
-					<?php if ( ! Auth::is_logged_in()): ?>
-						<div class="signin-panel corner-upper-left corner-lower-left corner-lower-right">
-							<?php echo form_open('login/check_login');?>
-								<table>
-									<tbody>
-										<tr>
-											<td>
-												<?php echo ucwords(lang('labels_email_address'));?><br>
-												<input type="text" name="email" class="signin-panel-input">
-											</td>
-											<td>
-												<?php echo ucfirst(lang('labels_password'));?><br>
-												<input type="password" name="password" class="signin-panel-input">
-											</td>
-											<td class="align_bottom"><?php echo form_button($button_login);?></td>
-										</tr>
-										<tr>
-											<td>
-												<input id="remember" type="checkbox" name="remember" value="yes">
-												<label for="remember"><?php echo ucfirst(lang('actions_remember').' '.lang('labels_me'));?></label>
-											</td>
-											<td><?php echo anchor('login/reset_password', lang('login_forgot'));?></td>
-											<td></td>
-										</tr>
-									</tbody>
-								</table>
-							<?php echo form_close();?>
-						</div>
-					<?php else: ?>
-						<a href="<?php echo site_url('login/logout');?>" class="signin corner-upper-left corner-upper-right corner-lower-left corner-lower-right"><?php echo ucfirst(lang('actions_logout'));?></a>
-						
-						<div class="logged-in-controls">
-							<?php if (Auth::is_logged_in()): ?>
-								<?php echo panel_inbox(true, true, false, '(x)', img($panel['inbox']));?> &nbsp;
-								<?php echo panel_writing(true, true, false, '(x)', img($panel['writing']));?> &nbsp;
-								<?php echo panel_dashboard(false, img($panel['dashboard']));?>
-							<?php endif;?>
-						</div>
-					<?php endif;?>
-				</div>
-			
-				<div style="clear:both;"></div>
-				
-				<div id="menu">
-					<div class="nav-main">
-						<?php echo $nav_main;?>
-					</div>
-				</div>
+			<div class="container">
+				<a class="brand pull-left">USS Nimitz NXI-3</a>
+
+				<div class="nav-main pull-right"><?php echo $nav_main;?></div>
 			</div>
 		</header>
 		
-		<div class="wrapper">
-			<div id="body">
-				<div class="nav-sub">
-					<?php echo $nav_sub;?>
-				</div>
-			
-				<div class="content">
+		<div class="container">
+			<div class="row">
+				<div class="span10">
 					<?php echo $flash_message;?>
 					<?php echo $content;?>
 					<?php echo $ajax;?>
-				
-					<div style="clear:both;"></div>
 				</div>
-				
-				<footer>
-					Powered by <strong><?php echo APP_NAME;?></strong> from <a href="http://www.anodyne-productions.com" target="_blank">Anodyne Productions</a> | 
-					<?php echo anchor('main/credits', 'Site Credits');?>
-				</footer>
+
+				<div class="span2">
+					<div class="nav-sub">
+						<?php echo $nav_sub;?>
+					</div>
+				</div>
 			</div>
 		</div>
+
+		<footer>
+			Powered by <?php echo APP_NAME;?> from <a href="http://www.anodyne-productions.com" target="_blank">Anodyne Productions</a> | 
+			<?php echo anchor('main/credits', 'Site Credits');?>
+		</footer>
 	</body>
 </html>
