173fb50d07455f00a021c4a343fd8ebd57532abc
David VanScott <david.vanscott@gmail.com>
2013-01-16 17:25:30 -0500

    Make sure the Nova config file starts in the core

--- application/config/nova.php
+++ application/config/nova.php
@@ -1,4 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once MODPATH.'core/config/nova_nova.php';
+
 /*
 | -------------------------------------------------------------------
 |  GENRE SELECTION
@@ -30,23 +33,6 @@
 */
 
 $config['genre'] = "DS9";
-
-/*
-| -------------------------------------------------------------------
-|  LAYOUT HEAD INCLUDE FILES
-| -------------------------------------------------------------------
-| One of the few required pieces of the system layout are the include
-| files. These are files that pull in the system's necessary stylesheets
-| and Javascript files. These are used globally by the entire system,
-| so modifying them can cause things to break. If your skin is going
-| to use a different or new file, it's recommended that you hard code
-| that into your specific skin and not into the global includes.
-*/
-
-$config['include_head_main']	= MODFOLDER.'/assets/include_head_main.php';
-$config['include_head_wiki']	= MODFOLDER.'/assets/include_head_wiki.php';
-$config['include_head_admin']	= MODFOLDER.'/assets/include_head_admin.php';
-$config['include_head_login']	= MODFOLDER.'/assets/include_head_login.php';
 
 /*
 | -------------------------------------------------------------------
---
+++ nova/modules/core/config/nova_nova.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+|  LAYOUT HEAD INCLUDE FILES
+| -------------------------------------------------------------------
+| One of the few required pieces of the system layout are the include
+| files. These are files that pull in the system's necessary stylesheets
+| and Javascript files. These are used globally by the entire system,
+| so modifying them can cause things to break. If your skin is going
+| to use a different or new file, it's recommended that you hard code
+| that into your specific skin and not into the global includes.
+*/
+
+$config['include_head_main']	= MODFOLDER.'/assets/include_head_main.php';
+$config['include_head_wiki']	= MODFOLDER.'/assets/include_head_wiki.php';
+$config['include_head_admin']	= MODFOLDER.'/assets/include_head_admin.php';
+$config['include_head_login']	= MODFOLDER.'/assets/include_head_login.php';
+
+$config['include_head_main_bs2']	= MODFOLDER.'/assets/include_head_main_bs2.php';
+$config['include_head_wiki_bs2']	= MODFOLDER.'/assets/include_head_wiki_bs2.php';
+$config['include_head_admin_bs2']	= MODFOLDER.'/assets/include_head_admin_bs2.php';
+$config['include_head_login_bs2']	= MODFOLDER.'/assets/include_head_login_bs2.php';
