5f5a565cdf631e93a36447232fc2fecf5e8659db
David VanScott <david.vanscott@gmail.com>
2013-01-16 17:24:49 -0500

    Keep working on the new default skin

--- application/views/default/template_main.php
+++ application/views/default/template_main.php
@@ -1,7 +1,6 @@
 <?php
 
 $section = 'main';
-$css = 'main.css';
 
 $path = explode('/', dirname(__FILE__));
 
@@ -15,7 +14,7 @@ $currentSkin = $path[$skinLoc];
 
 // set up the link tag parameters
 $link = array(
-	'href'		=> APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/css/'. $css,
+	'href'		=> APPFOLDER."/views/$currentSkin/$section/css/main.css",
 	'rel'		=> 'stylesheet',
 	'type'		=> 'text/css',
 	'media'		=> 'screen',
@@ -27,9 +26,9 @@ $this->load->helper('panel');
 
 // set up the locations of the icons
 $panel = array(
-	'inbox'		=> array('src' => APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/images/panel-mail.png'),
-	'writing'	=> array('src' => APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/images/panel-writing.png'),
-	'dashboard'	=> array('src' => APPFOLDER.'/views/'.$currentSkin.'/'.$section.'/images/panel-dashboard.png'),
+	'inbox'		=> array('src' => APPFOLDER."/views/$currentSkin/$section/images/panel-mail.png"),
+	'writing'	=> array('src' => APPFOLDER."/views/$currentSkin/$section/images/panel-writing.png"),
+	'dashboard'	=> array('src' => APPFOLDER."/views/$currentSkin/$section/images/panel-dashboard.png"),
 );
 
 $loginButton = array(
@@ -52,20 +51,85 @@ $loginButton = array(
 		
 		<?php echo $_redirect;?>
 
-		<?php include_once $this->config->item('include_head_main');?>
-		
-		<?php echo link_tag($link);?>
-		
 		<!--[if lt IE 9]>
 		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
+
+		<?php include_once MODFOLDER.'/assets/global/main_head.php';?>
 		
+		<?php echo link_tag($link);?>
+	</head>
+	<body>
+		<div id="wrap">
+			<noscript>
+				<div class="system_warning"><?php echo lang_output('text_javascript_off', '');?></div>
+			</noscript>
+			
+			<?php if (Auth::is_logged_in()): ?>
+				<div id="panel" class="hidden">
+					<div class="panel-body">
+						<table class="table100">
+							<tbody>
+								<tr>
+									<td class="panel_1 align_top"><?php echo $panel_1;?></td>
+									<td class="panel_spacer"></td>
+									<td class="panel_2 align_top"><?php echo $panel_2;?></td>
+									<td class="panel_spacer"></td>
+									<td class="panel_3 align_top"><?php echo $panel_3;?></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			<?php endif;?>
+
+			<header>
+				<div class="container">
+					<a class="brand pull-left"><?php echo $this->options['sim_name'];?></a>
+
+					<div class="nav-main pull-right"><?php echo $nav_main;?></div>
+				</div>
+			</header>
+			
+			<div class="container body-container">
+				<div class="row">
+					<div class="span10">
+						<?php echo $flash_message;?>
+						<?php echo $content;?>
+						<?php echo $ajax;?>
+					</div>
+
+					<div class="span2 align_right">
+						<?php if (Auth::is_logged_in()): ?>
+							<div class="row-fluid user-controls">
+								<div class="span4"><?php echo panel_dashboard(false, img($panel['dashboard']));?></div>
+								<div class="span4"><?php echo panel_inbox(true, true, false, '(x)', img($panel['inbox']));?></div>
+								<div class="span4"><?php echo panel_writing(true, true, false, '(x)', img($panel['writing']));?></div>
+							</div>
+						<?php endif;?>
+
+						<div class="nav-sub">
+							<?php echo $nav_sub;?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<footer>
+				Powered by <?php echo APP_NAME;?> from <a href="http://www.anodyne-productions.com" target="_blank">Anodyne Productions</a> | 
+				<?php echo anchor('main/credits', 'Site Credits');?>
+			</footer>
+		</div>
+
+		<?php include_once MODFOLDER.'/assets/global/main_foot.php';?>
+
 		<script type="text/javascript" src="<?php echo base_url().APPFOLDER;?>/views/<?php echo $currentSkin;?>/jquery.blockUI.js"></script>
 		
 		<?php echo $javascript;?>
 		
 		<script type="text/javascript">
 			$(document).ready(function(){
+				
 				$('body').click(function(event){
 					if (! $(event.target).closest('div').hasClass('signin-panel'))
 					{
@@ -98,57 +162,5 @@ $loginButton = array(
 				}
 			});
 		</script>
-	</head>
-	<body>
-		<noscript>
-			<div class="system_warning"><?php echo lang_output('text_javascript_off', '');?></div>
-		</noscript>
-		
-		<?php if (Auth::is_logged_in()): ?>
-			<div id="panel" class="hidden">
-				<div class="panel-body">
-					<table class="table100">
-						<tbody>
-							<tr>
-								<td class="panel_1 align_top"><?php echo $panel_1;?></td>
-								<td class="panel_spacer"></td>
-								<td class="panel_2 align_top"><?php echo $panel_2;?></td>
-								<td class="panel_spacer"></td>
-								<td class="panel_3 align_top"><?php echo $panel_3;?></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		<?php endif;?>
-
-		<header>
-			<div class="container">
-				<a class="brand pull-left">USS Nimitz NXI-3</a>
-
-				<div class="nav-main pull-right"><?php echo $nav_main;?></div>
-			</div>
-		</header>
-		
-		<div class="container">
-			<div class="row">
-				<div class="span10">
-					<?php echo $flash_message;?>
-					<?php echo $content;?>
-					<?php echo $ajax;?>
-				</div>
-
-				<div class="span2">
-					<div class="nav-sub">
-						<?php echo $nav_sub;?>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<footer>
-			Powered by <?php echo APP_NAME;?> from <a href="http://www.anodyne-productions.com" target="_blank">Anodyne Productions</a> | 
-			<?php echo anchor('main/credits', 'Site Credits');?>
-		</footer>
 	</body>
 </html>
